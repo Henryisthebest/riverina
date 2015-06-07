c04bcf616eb48cfb1b3cf780a37004e4743492a7
Henryisthebest <ginil@live.com>
2015-06-07 14:04:14 +0930

    Update index.js

--- js/index.js
+++ js/index.js
@@ -63,18 +63,6 @@ var app = {
         //console.log("object is : " + push)
     
         console.log("Adding new tag: " + new_tag)
-        window.plugins.OneSignal.getTags(function(obj) {
-            console.log('Tags Received: ' + JSON.stringify(tags));
-            if (obj.tags.indexOf({new_tag, 'yes'}) == -1) {
-                alert("Valid tag: " + new_tag)
-                obj.tags = obj.tags.concat([new_tag, 'yes']);
-                alert('obj.tags is' + obj.tags);
-                window.plugins.OneSignal.getTags.setTags(obj.tags, function() {
-                    $('#tags').prepend("<p>" + new_tag + "<input type='button' class='removeTagButton' value='Remove' /></p>");
-                    $("#addTagField").val('')
-                });
-                
-            }
-        });
+        
     }
 };
